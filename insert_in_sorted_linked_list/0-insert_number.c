c49a8b1d364c96109aace7efe7d23718048f3bdc
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-09-03 16:14:29 -0400

    check if the node can go in the first position

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -19,26 +19,27 @@ listint_t *insert_node(listint_t **head, int number)
     // assign nodes
     new = malloc(sizeof(listint_t));
     current = *head;
-    // check that the link list is not null
-    if (current == NULL)
-    {
-        return NULL;
-    }
-
     // Check if malloc succeeded
     if (new == NULL)
     {
         return NULL;
+    }
+    // Assign the value of number to the new node
+    new->n = number;
+
+    if (!current || current->n >= number)
+    {
+        new->next = *head;
+        *head = new;
+        return new;
     }
 
     // Traverse the linked list
     while (current != NULL && current->next != NULL)
     {
         // Check if the current position is the correct insertion point
-        if (current->n < number && current->next->n >= number)
+        if (current->next->n > number)
         {
-            // Assign the value of number to the new node
-            new->n = number;
             // Connect the new node to the next node
             new->next = current->next;
             // Connect the current node to the new node
@@ -49,6 +50,8 @@ listint_t *insert_node(listint_t **head, int number)
         // Move to the next node
         current = current->next;
     }
-    // Return NULL if the correct position is not found
-    return NULL;
+    // connect current to new
+    current->next = new;
+    // Return the new node
+    return new;
 }
