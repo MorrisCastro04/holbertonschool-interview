a4e3aa4fce3a4291e934e95ec63690a47c0f849b
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-09-09 19:24:58 -0400

    betty coding style

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -4,7 +4,6 @@
 
 /**
  * binary_tree_node - creates a binary tree node
- *
  * @parent: pointer to the parent node of the node to create
  * @value: value to put in the new node
  *
@@ -12,8 +11,9 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+    binary_tree_t *new_node;
 
+    new_node = malloc(sizeof(binary_tree_t));
     if (new_node == NULL)
         return (NULL);
 
